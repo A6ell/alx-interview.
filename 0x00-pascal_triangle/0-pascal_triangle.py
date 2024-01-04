94ba0573df2fad1d15f7854107dde4f2f5c8a758
Ab-Ruh <abell.demere@gmail.com>
2024-01-04 17:16:01 +0300

    triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,34 +1,23 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
+
 def pascal_triangle(n):
     """
-    Generate Pascal's Triangle up to the nth row.
-
-    Args:
-        n (int): The number of rows to generate.
-
-    Returns:
-        list of lists: A list of lists representing Pascal's Triangle.
+         returns a list of lists of
+         integers representing
+          the Pascal’s triangle of n
+         Returns an empty list if n <= 0
     """
     if n <= 0:
         return []
-
     triangle = [[1]]
-
     for i in range(1, n):
-        row = [1]  # First element in each row is always 1
+        row = [1]
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)  # Last element in each row is always 1
+        row.append(1)
         triangle.append(row)
-
     return triangle
-
-# Example usage:
-# Uncomment the following lines if you want to test the function using the
-# provided main script.
-
-# def print_triangle(triangle):
-#     for row in triangle:
-#         print("[{}]".format(",".join([str(x) for x in row])))
-#
-# if __name__ == "__main__":
-#     print_triangle(pascal_triangle(5))
