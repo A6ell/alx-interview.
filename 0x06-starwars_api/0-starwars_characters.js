ca6aff40e5f172d8fe59ea96743d9d82f0019c7b
ab-ruh <abell.demere@gmail.com>
2024-02-15 19:13:56 +0300

    star

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,29 @@
 #!/usr/bin/node
-const util = require('util')
-const request = util.promisify(require('request'))
-const filmID = process.argv[2]
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
 
 async function starwarsCharacters(filmId) {
-    const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId
-    let response = await (await request(endpoint)).body
-    response = JSON.parse(response)
-    const characters = response.characters
+  const endpoint = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+  try {
+    const response = await request(endpoint);
+    const filmData = JSON.parse(response.body);
+    const characters = filmData.characters;
 
-    for (let i=0
-         i < characters.length
-         i + +) {
-        const urlCharacter = characters[i]
-        let character = await (await request(urlCharacter)).body
-        character = JSON.parse(character)
-        console.log(character.name)
+    for (let i = 0; i < characters.length; i++) {
+      const urlCharacter = characters[i];
+      try {
+        const characterResponse = await request(urlCharacter);
+        const character = JSON.parse(characterResponse.body);
+        console.log(character.name);
+      } catch (characterError) {
+        console.error(`Error fetching character: ${characterError.message}`);
+      }
     }
+  } catch (error) {
+    console.error(`Error fetching film data: ${error.message}`);
+  }
 }
 
-starwarsCharacters(filmID)
+starwarsCharacters(filmID);
+
