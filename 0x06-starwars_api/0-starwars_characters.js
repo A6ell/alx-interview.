a13914a8393e49c591d00022ab53e26147238a27
ab-ruh <abell.demere@gmail.com>
2024-02-15 20:19:09 +0300

    star

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,7 +3,7 @@ const util = require('util');
 const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
-async function starwarsCharacters(filmId) {
+async function starwarsCharacters (filmId) {
   const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
   try {
     const response = await request(endpoint);
@@ -26,4 +26,3 @@ async function starwarsCharacters(filmId) {
 }
 
 starwarsCharacters(filmID);
-
