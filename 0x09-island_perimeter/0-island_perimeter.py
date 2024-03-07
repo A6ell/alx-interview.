42add963832f5880ef0b3b7abadc4b06e53c8f78
ab-ruh <abell.demere@gmail.com>
2024-03-07 17:19:28 +0300

    island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island in the grid.
+
+    Args:
+        grid (list of list of int): 2D grid representing the island.
+
+    Returns:
+        int: Perimeter of the island.
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                # Check left neighbor
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+                # Check top neighbor
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
+
